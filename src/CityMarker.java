204879caa62e091cf53560c226eac3e5e7da1f5d
Ronit Chattopadhyay <ronit0717@gmail.com>
2016-07-05 14:54:44 +0530

    Add files via upload

---
+++ src/CityMarker.java
@@ -0,0 +1,97 @@
+package module5;
+
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.geo.Location;
+import processing.core.PGraphics;
+
+/** Implements a visual marker for cities on an earthquake map
+ * 
+ * @author UC San Diego Intermediate Software Development MOOC team
+ * @author Ronit Chattopadhyay
+ *
+ */
+// TODO: Change SimplePointMarker to CommonMarker as the very first thing you do 
+// in module 5 (i.e. CityMarker extends CommonMarker).  It will cause an error.
+// That's what's expected.
+public class CityMarker extends CommonMarker {
+	
+	public static int TRI_SIZE = 5;  // The size of the triangle marker
+	
+	public CityMarker(Location location) {
+		super(location);
+	}
+	
+	
+	public CityMarker(Feature city) {
+		super(((PointFeature)city).getLocation(), city.getProperties());
+		// Cities have properties: "name" (city name), "country" (country name)
+		// and "population" (population, in millions)
+	}
+	
+
+	
+	public boolean checkOcean(){
+		return false;
+	}
+	
+
+	
+	/**
+	 * Implementation of method to draw marker on the map.
+	 */
+	
+	
+	
+	/** The drawMarker method that needs to be implemented being abstract in the super class */
+	public void drawMarker(PGraphics pg, float x, float y) {
+		// Save previous drawing style
+		pg.pushStyle();
+		
+		// IMPLEMENT: drawing triangle for each city
+		pg.fill(150, 30, 30);
+		pg.triangle(x, y-TRI_SIZE, x-TRI_SIZE, y+TRI_SIZE, x+TRI_SIZE, y+TRI_SIZE);
+		// Restore previous drawing style
+		pg.popStyle();
+	}
+	
+	/** Show the title of the city if this marker is selected */
+	public void showTitle(PGraphics pg, float x, float y)
+	{
+		
+		// TODO: Implement this method
+		pg.pushStyle();
+		
+		Float population=getPopulation();
+		String city = getCity();
+		String country = getCountry();
+		pg.fill(0);
+		pg.rect(x+2f,y-10f,500,40);
+		pg.fill(255,255,255);
+		pg.text("City: "+city,x+2f,y+2f);
+		pg.text("Country: "+country, x+2f,y+12f);
+		pg.text("Population: "+population, x+2f,y+22f);
+		
+		pg.popStyle();
+		
+	}
+	
+	
+	
+	/* Local getters for some city properties.  
+	 */
+	public String getCity()
+	{
+		return getStringProperty("name");
+	}
+	
+	public String getCountry()
+	{
+		return getStringProperty("country");
+	}
+	
+	public float getPopulation()
+	{
+		return Float.parseFloat(getStringProperty("population"));
+	}
+}
